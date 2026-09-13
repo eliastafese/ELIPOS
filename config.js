9ec1fccd8abf7687da4fa4694f0b82ac43fb31c2
elias tafese <eliastafese08@gmail.com>
2026-09-13 22:17:41 +0100

    Update store and footer messages to Amharic

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 window.POS_CONFIG = {
   SUPABASE_URL: "https://vaqoxdjfegzgqbdyihsh.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcW94ZGpmZWd6Z3FiZHlpaHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNzAwMTAsImV4cCI6MjEwNDg0NjAxMH0.SHpSMBxbhMgab_SmZbDuk0XM4-JuiSVxRtKfhs2gQ3o",
-  STORE_NAME: "My Store",       // ← rename to your store
+  STORE_NAME: "መተግበሪያ ለመፍትሔ",       // ← rename to your store
   TAX_NAME: "Tax",
   TAX_RATE: 0.0825,             // matches your workbook's Admin sheet
   LOW_STOCK: 10,
-  FOOTER_MESSAGE: "Thank You For Your Business",
+  FOOTER_MESSAGE: "እናመሠግናለን እባክሆን በደንበኝነቶዎ ይቀጥሉ ",
   CURRENCY: "$"
 };
