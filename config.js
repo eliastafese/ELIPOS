e13b87b0e6524b039c316463b0ac1d771c3493b5
elias tafese <eliastafese08@gmail.com>
2026-09-19 20:54:20 +0100

    Change TAX_RATE from 0.0825 to 0.015
    Updated the tax rate to reflect the new value.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.POS_CONFIG = {
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcW94ZGpmZWd6Z3FiZHlpaHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNzAwMTAsImV4cCI6MjEwNDg0NjAxMH0.SHpSMBxbhMgab_SmZbDuk0XM4-JuiSVxRtKfhs2gQ3o",
   STORE_NAME: "My Store",       // ← rename to your store
   TAX_NAME: "Tax",
-  TAX_RATE: 0.0825,             // matches your workbook's Admin sheet
+  TAX_RATE: 0.015,             // matches your workbook's Admin sheet
   LOW_STOCK: 10,
   FOOTER_MESSAGE: "እናመሠግናለን እባክሆን በደንበኝነቶዎ ይቀጥሉ ",
   CURRENCY: "$"
